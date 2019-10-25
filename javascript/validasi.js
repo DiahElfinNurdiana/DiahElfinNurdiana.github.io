5d620fb3d4e2dbcd30160e818f84a4db9eaf1948
ZhafirahNabillah <55244721+ZhafirahNabillah@users.noreply.github.com>
2019-10-25 09:56:37 +0700

    validasi js login

--- javascript/validasi.js
+++ javascript/validasi.js
@@ -3,7 +3,7 @@ var pass = document.getElementById("password");
 var tanggapan = document.getElementById("tanggapan");
 function validate(){
   if(pass.value.length<=8){
-    tanggapan.innerHTML = "Password harus 8 karakter";
+    tanggapan.innerHTML = "Password harus 7 karakter";
   }else{
     tanggapan.innerHTML = '';
   }
